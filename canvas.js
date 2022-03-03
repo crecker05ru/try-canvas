377a6aa7e48d3672171f83e487e1a1611b113c5b
Anvar <66783845+crecker05ru@users.noreply.github.com>
2022-03-03 17:22:28 +0300

    start

---
+++ canvas.js
@@ -0,0 +1,669 @@
+
+let startButton = document.querySelector('.start-button')
+let stopButton = document.querySelector('.stop-button')
+let switchButton = document.querySelector('.switch-button')
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+console.log('ctx',ctx)
+        // ctx.moveTo(50, 200);
+        // ctx.lineTo(800, 400);
+let timer
+        // let count = 10
+        // let startX = 60
+        // let startY = 200
+        // ctx.moveTo(startX,startY);
+        // ctx.lineWidth = '3'
+        // ctx.strokeStyle = 'orange'
+        
+function startDraw (){
+        ctx.clearRect(0,0,800,400)
+        ctx.beginPath()
+        let count = 10
+        let startX = 60
+        let startY = 200
+        
+        ctx.moveTo(startX,startY);
+        ctx.lineWidth = '3'
+        ctx.strokeStyle = 'orange'
+            for(i = 0;i <= count; i++){
+            ctx.lineTo(startX += 60, startY = startY > 200 ? startY -= Math.random() * 50 
+            : startY += Math.random() * 50 )  ;
+            ctx.stroke()
+            console.log('i',i)
+            // ctx.beginPath()
+             }
+            //  count = 0
+        timer = setTimeout(startDraw,1000/5)
+        
+    }
+function fromTenToTwo () {
+        ctx.clearRect(0,0,800,400)
+        ctx.beginPath()
+        let count = 10
+        let startX = 60
+        let startY = 200
+        ctx.moveTo(startX,startY);
+        ctx.lineWidth = '2'
+        ctx.strokeStyle = 'orange'
+            for(i = 0;i <= Math.random()*10 + 2; i++){
+            ctx.lineTo(startX += 60, startY = startY > 200 ? startY -= Math.random() * 50 
+            : startY += Math.random() * 50 )  ;
+            ctx.stroke()
+            // ctx.beginPath(startX+i*30,startY+i*30)
+            // ctx.arc(startX+i*30,startY+i*30,10,0,Math.PI+(Math.PI*1),false)
+            // ctx.fill()
+            console.log('i',i)
+             }
+    }
+// let drowedGraph = fromTenToTwo()
+
+        // startDraw()
+        // let timer = setTimeout(startDraw,1000/30)
+        
+        // setInterval(() => {
+        //     startDraw()
+        // },60)
+        startButton.addEventListener('click',
+            startDraw
+        )
+         function stopDraw () {
+             startDraw = null
+             startButton.removeEventListener('click',startDraw)
+         }
+        stopButton.addEventListener('click',stopDraw)
+        // stopButton.removeEventListener('click',stopDraw)
+        
+        function switchLines () {
+        ctx.clearRect(0,0,800,400)
+        ctx.beginPath()
+        let count = 10
+        let startX = 60
+        let startY = 200
+        
+        ctx.moveTo(startX,startY);
+        ctx.lineWidth = '3'
+        ctx.strokeStyle = 'orange'
+            for(i = 0;i <= count; i++){
+            ctx.lineTo(startX += 60, startY = startY > 200 ? startY -= Math.random() * 50 
+            : startY += Math.random() * 50 )  ;
+            ctx.stroke()
+            console.log('i',i)
+            // ctx.beginPath()
+             }
+        }
+
+// switchButton.addEventListener('click',drowedGraph)
+
+// function lineAndCircle (){
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(50,200)
+//     ctx.lineTo(100,150)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(100,150,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(100,150)
+//     ctx.lineTo(150,250)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(150,250,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(150,250)
+//     ctx.lineTo(200,300)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(200,300,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(200,300)
+//     ctx.lineTo(250,200)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(250,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(250,200)
+//     ctx.lineTo(300,200)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(300,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(300,200)
+//     ctx.lineTo(350,150)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(350,150,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(350,150)
+//     ctx.lineTo(400,180)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(400,180,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(400,180)
+//     ctx.lineTo(450,200)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(450,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(450,200)
+//     ctx.lineTo(500,240)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(500,240,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(500,240)
+//     ctx.lineTo(550,200)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(550,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(550,200)
+//     ctx.lineTo(600,170)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(600,170,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     //Begin
+//     ctx.beginPath()  //first circle
+//     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     ctx.beginPath() // line
+//     ctx.moveTo(600,170)
+//     ctx.lineTo(650,200)
+//     ctx.stroke()
+//     ctx.beginPath() // last circle
+//     ctx.arc(650,200,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     //end
+//     }
+// lineAndCircle()
+
+// function lineAndCircle (x = 1,y = 1,radius = 5,startAngle = 0,endAngle = Math.PI+Math.PI*1,ainticlockwise = false){
+//     ctx.beginPath()
+//     ctx.moveTo(x + 50*x,y + 200)
+//     ctx.lineTo(x + 60*x, y = y > 200 ? y -= Math.random() * 50 
+//     : y += Math.random() * 50 )
+//     ctx.stroke()
+//     ctx.beginPath()
+//     ctx.arc(70+1*30,120+1*30,5,0,Math.PI+(Math.PI*1),false)
+//     ctx.lineWidth = '1'
+//     ctx.strokeStyle = "#000000"
+//     // ctx.strokeStyle = "#ffffff"
+//     ctx.stroke()
+//     ctx.fillStyle = "#FFA500"
+//     }
+
+function lineAndCircle (coordinates,radius = 5,startAngle = 0,endAngle = Math.PI+Math.PI*1,ainticlockwise = false){
+    //Begin
+    ctx.beginPath()
+    ctx.moveTo(50,200)
+    ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+    ctx.lineWidth = '1'
+    ctx.strokeStyle = "#000000"
+    ctx.fillStyle = "#FFA500"
+    for (const [x,y] of coordinates){
+        ctx.lineTo(x,y)
+        // ctx.beginPath()  //first circle
+        ctx.arc(x,y,5,0,Math.PI+(Math.PI*1),false)
+    }
+    ctx.stroke()
+    ctx.closePath()
+    //end
+}
+
+// function drawGraphWithLines (){
+//     for(i = 1;i <= 16;i++){
+//         lineAndCircle(i)
+//     }
+// }
+// drawGraphWithLines ()
+
+// lineAndCircle([
+//     [100,150],
+//     [150,250],
+//     [200,300],
+//     [250,250],
+//     [300,230],
+//     [350,200],
+//     [400,180],
+//     [450,100],
+//     [500,200],
+//     [550,250],
+//     [600,180],
+//     [650,230],
+//     [700,350],
+//     [750,300]
+// ])
+// lineAndCircle([
+//     [200,150],
+//     [300,250],
+//     [400,300],
+//     [500,250],
+//     [600,230],
+//     [700,200],
+//     [750,180],
+// ])
+
+// function lineFromTo(from,to){
+//     //Begin
+//     let [fromX,fromY] = from
+//     let [toX, toY] = to
+//     let current = [fromX,fromY] 
+//     while(current[0] < toX || current[1] < toY){
+//         ctx.beginPath()
+//         ctx.clearRect(0,0,800,800)
+//         // ctx.save()
+//         ctx.moveTo(fromX, fromY)
+//         ctx.lineTo(current[0],current[1])
+//         current[0] += current[0] < toX ? 1 : 0
+//         current[1] += current[1] < toY ? 1 : 0
+//         ctx.stroke()
+//         // ctx.restore()
+//     }
+//     // ctx.closePath()
+//     //end
+// }
+
+function lineFromToAnimation(from,to){
+    //Begin
+    let [fromX,fromY] = from
+    let [toX, toY] = to
+    let current = [fromX,fromY]
+    // current[0] +=1
+    // current[1] += 1
+    function draw (){
+        if(current[0] < toX || current[1] < toY){
+        ctx.clearRect(0,0,800,800)   
+        current[0] += current[0] < toX ? 1 : 0
+        current[1] += current[1] < toY ? 1 : 0
+        ctx.arc(current[0],current[1],5,0,Math.PI+(Math.PI*1),false)
+        ctx.stroke()
+        }
+        
+        ctx.beginPath()
+        // ctx.clearRect(0,0,800,800)
+        // ctx.save()
+        ctx.moveTo(fromX, fromY)
+        ctx.lineTo(current[0],current[1])
+        // current[0] += current[0] < toX ? 1 : 0
+        // current[1] += current[1] < toY ? 1 : 0
+        // ctx.stroke()
+        // ctx.restore() 
+        setTimeout(draw,1000/60)
+}
+draw()
+    // ctx.closePath()
+    //end
+}
+// lineFromToAnimation([50,150],[300,200])
+
+// function DrawGraphFromTo(from = [[0,0]], to = [[100,100]]){
+//     //Begin
+//     let [fromX,fromY] = from
+//     let [toX, toY] = to
+//     // let current = [fromX,fromY]
+//     console.log('from',from)
+//     console.log('fromX',fromX)
+
+//     // function drawGraph(){
+//     //     ctx.beginPath()
+//     //     ctx.moveTo(50,200)
+//     //     ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//     //     for (const [x,y] of from){
+//     //         ctx.lineTo(x,y)
+//     //         // ctx.beginPath()  //first circle
+//     //         ctx.arc(x,y,5,0,Math.PI+(Math.PI*1),false)
+//     //     }
+//     //     ctx.moveTo(50,200)
+//     //     for (const [x,y] of to){
+//     //         ctx.lineTo(x,y)
+//     //         // ctx.beginPath()  //first circle
+//     //         ctx.arc(x,y,5,0,Math.PI+(Math.PI*1),false)
+//     //     }
+//     // }
+    
+//     function drawAnimation (){
+//         ctx.beginPath()
+//         ctx.moveTo(50,200)
+//         ctx.arc(50,200,5,0,Math.PI+(Math.PI*1),false)
+//         ctx.stroke()
+//         for (const [toX,toY] of to){
+//             // console.log('to',to)
+//             // console.log('toX',toX)
+//             for(const [fromX,fromY] of from){
+//                 let currentX = fromX
+//                 let currentY = fromY
+//                 // console.log('currentX',currentX)
+//                 // console.log('currentY',currentY)
+//                 if(currentX < toX || currentY < toY){
+//                     console.log('to',to)
+//                     console.log('current[0]',currentY)
+//                     console.log('current[1]',currentY)
+//                     ctx.clearRect(0,0,800,800)   
+//                     currentX += currentX < toX ? 1 : 0
+//                     currentY += currentY < toY ? 1 : 0
+//                     ctx.arc(currentX,currentY,5,0,Math.PI+(Math.PI*1),false)
+//                     ctx.stroke()
+//                     // ctx.beginPath()
+//                     // ctx.moveTo(fromX, fromY)
+//                     ctx.lineTo(currentX,currentY)
+//                     ctx.stroke()
+//                     }
+//             }
+//         ctx.lineTo(toX,toY)
+//         ctx.beginPath()
+//         // ctx.arc(toX,toY,5,0,Math.PI+(Math.PI*1),false)
+//         }
+//         // drawGraph()
+
+//         setTimeout(drawAnimation,1000/5)
+//     }
+//     drawAnimation()
+//     //end
+// }
+
+// DrawGraphFromTo(
+//     [
+//         [100,150],
+//         [150,250],
+//         [200,300],
+//         [250,250],
+//         [300,230],
+//         [350,200],
+//         [400,180],
+//         [450,100],
+//         [500,200],
+//         [550,250],
+//         [600,180],
+//         [650,230],
+//         [700,350],
+//         [750,300]
+//     ],
+//     [
+//     [200,150],
+//     [300,250],
+//     [400,300],
+//     [500,250],
+//     [600,230],
+//     [700,200],
+//     [750,180],
+//     ]
+//     )
+let inputArr = [
+        [
+        [100,150],
+        [150,250],
+        [200,300],
+        [250,250],
+        [300,230],
+        [350,200],
+        [400,180],
+        [450,100],
+        [500,200],
+        [550,250],
+        [600,180],
+        [650,230],
+        [700,350],
+        [750,300]
+    ],
+    [
+    [200,150],
+    [300,250],
+    [400,300],
+    [500,250],
+    [600,230],
+    [700,200],
+    [750,180],
+    ]
+]
+let inputData = {
+        from: [
+        [100,150],
+        [150,250],
+        [200,300],
+        [250,250],
+        [300,230],
+        [350,200],
+        [400,180],
+        [450,100],
+        [500,200],
+        [550,250],
+        [600,180],
+        [650,230],
+        [700,350],
+        [750,300]
+        ],
+        to: [
+        [200,150],
+        [300,250],
+        [400,300],
+        [500,250],
+        [600,230],
+        [700,200],
+        [750,180],    
+        ]
+}
+
+function drawLineAndCircle([x,y]){
+        ctx.clearRect(0,0,800,400)
+        ctx.lineTo(x,y)
+        ctx.arc(x,y,5,0,Math.PI+(Math.PI*1),false)
+        ctx.stroke()
+}
+function drawLineAndCircleFromTo([fromX,fromY],[toX,toY]){
+    let currentX = fromX
+    let currentY = fromY
+    console.log('currentX',currentX)
+    function lineAnimation(){
+        if(currentX < toX || currentY < toY){
+            ctx.clearRect(0,0,800,400)
+            ctx.beginPath()
+            ctx.moveTo(fromX,fromY)
+            currentX += currentX < toX ? 1 : 0
+            currentY += currentY < toY ? 1 : 0
+            ctx.lineTo(currentX ,currentY)
+            ctx.arc(currentX,currentY ,5,0,Math.PI+(Math.PI*1),false)   
+            ctx.stroke()
+        }
+        
+        setTimeout(lineAnimation,1000/40)
+    }
+    lineAnimation()
+}
+drawLineAndCircleFromTo([100,220],[250,320])
+
+function moveFromTo([fromX,fromY],[toX,toY]) {
+    let currentX = fromX
+    let currentY = fromY
+    currentX += currentX < toX ? 1 : 0
+    currentY += currentY < toY ? 1 : 0
+  };
+function moveFrom([fromX,fromY]){
+    let currentX = fromX
+    let currentY = fromY
+    currentX += currentX < ctx.canvas.width ? 1 : 0
+    currentY += currentY < ctx.canvas.width ? 1 : 0
+}
+function moveTo(x,y){
+    x += x < ctx.canvas.width ? 1 : 0
+    y += y < ctx.canvas.width ? 1 : 0
+  };
+// drawLineAndCircle(100,300)
+// drawLineAndCircle(150,330)
+// drawLineAndCircle(200,330)
+function drawGraph(){
+    window.requestAnimationFrame(drawGraph)
+    ctx.moveTo(50,200)
+    // inputData.from.forEach(drawLineAndCircle)
+    inputData.to.forEach(moveTo)
+
+    // ctx.moveTo(50,200)
+    // inputData.from.forEach(moveTo)
+}
+let dataFrom = [200,150]
+let dataTo = [300,150]
+function example(){
+    window.requestAnimationFrame(drawGraph)
+    ctx.moveTo(50,200)
+    // drawLineAndCircle(dataFrom)
+    // drawLineAndCircleFromTo(dataFrom,dataTo)
+    // moveTo(dataFrom[0],dataFrom[1])
+    
+}
+example()
+drawGraph()
+function lineAnimation(){
+    let [fromX,fromY] = from
+    let [toX, toY] = to
+    let current = [fromX,fromY]
+    function draw (){
+        if(current[0] < toX || current[1] < toY){
+            ctx.clearRect(0,0,800,800)   
+            current[0] += current[0] < toX ? 1 : 0
+            current[1] += current[1] < toY ? 1 : 0
+            ctx.arc(current[0],current[1],5,0,Math.PI+(Math.PI*1),false)
+            ctx.stroke()
+        }
+        ctx.beginPath()
+        ctx.moveTo(fromX, fromY)
+        ctx.lineTo(current[0],current[1])
+        setTimeout(draw,1000/20)
+    }
+
+}
